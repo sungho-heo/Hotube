d7aa2008e3b27302e2b75f321fcd32da72559f77
Sungho <hurgj123kr@gmail.com>
2023-04-09 20:20:28 +0900

    port heroku port use

--- src/init.js
+++ src/init.js
@@ -5,7 +5,7 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server.js";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 
 const handleAppListen = () => console.log(`good app:http://localhost:${PORT}/`);

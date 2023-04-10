271e0e7c65c76047722e267bfe5fc07e2fb6b955
Sungho <hurgj123kr@gmail.com>
2023-04-10 14:09:55 +0900

    avatar file s3 save

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,20 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import { S3Client }  from "@aws-sdk/client-s3";
+
+const s3 = new S3Client({
+    region: "ap-northeast-2",
+    credentials: {
+        accessKeyId: process.env.AWS_ID,
+        secretAccessKey: process.env.AWS_SECRET,
+    },
+});
+
+const multerUploder = multerS3({
+    s3: s3,
+    bucket: "ho-tube"
+});
+
 export const middleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn);
     res.locals.user = req.session.user || {};
@@ -23,8 +39,13 @@ export const publicMiddleware = (req, res, next) => {
     }
 };
 
-export const uploadAvatar = multer({ dest: "uploads/avatar", limits: {filesize: 30000} });
+export const uploadAvatar = multer({
+    dest: "uploads/avatar",
+    limits: { filesize: 30000 },
+    storage: multerUploder,
+});
 export const uploadVideo = multer({
     dest: "uploads/videos",
     limits: { filesize: 100000 },
+    storage: multerUploder,
 });
